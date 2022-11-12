1628edd91e9ba02360bda781e8c091fd8ad91d5e
jisungbin <ji@sungb.in>
2022-11-12 09:31:15 +0900

    feat: remove original text implementations

--- src/main/kotlin/land/sungbin/gfm/tag/text/Italic.kt
+++
@@ -1,9 +0,0 @@
-package land.sungbin.gfm.tag.text
-
-class Italic(private val text: String) : TextTag() {
-    override fun content(): String = "*$text*"
-}
-
-fun italic(text: String): Italic {
-    return Italic(text)
-}
--- src/main/kotlin/land/sungbin/gfm/tag/text/PlainText.kt
+++
@@ -1,9 +0,0 @@
-package land.sungbin.gfm.tag.text
-
-class PlainText(private val text: String) : TextTag() {
-    override fun content(): String = text
-}
-
-fun plainText(plainText: String): TextTag {
-    return PlainText(plainText)
-}
